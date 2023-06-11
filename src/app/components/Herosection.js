b494ff6b5e84bb143bcae66171357d94f892a852
Shivkumar Salunkhe <shivkumar.salunkhe@exaltaretech.com>
2023-06-12 00:26:42 +0530

    added gradient shape background

--- src/app/components/Herosection.js
+++ src/app/components/Herosection.js
@@ -2,17 +2,16 @@ import React from "react";
 import heroStyles from "@/app/styles/herosection.module.css";
 import styles from "@/app/styles/common.module.css";
 import Link from "next/link";
-import { Mulish } from 'next/font/google';
+import { Mulish } from "next/font/google";
 import Image from "next/image";
 
 const mulish = Mulish({
-    weight: ['300', '400', '500', '600', '700', '800', '900'],
-    subsets: ['latin'],
-    display: 'swap'
-})
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
-
-function Herosection({title, imageUrl}) {
+function Herosection({ title, imageUrl }) {
   return (
     <main className={heroStyles.main_section}>
       <div className={styles.container}>
@@ -30,9 +29,22 @@ function Herosection({title, imageUrl}) {
           </div>
 
           <div className={heroStyles.hero_image}>
-            <Image src={imageUrl} alt="Home Page" width={500} height={500}/>
+            <Image src={imageUrl} alt="Home Page" width={500} height={500} />
           </div>
         </div>
+      </div>
+      <div className={heroStyles["custom-shape-divider-bottom-1681647578"]}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            className={heroStyles["shape-fill"]}
+          ></path>
+        </svg>
       </div>
     </main>
   );
