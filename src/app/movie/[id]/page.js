e470ccaa55bada1e76ed518e7e531c628c679e44
Shivkumar Salunkhe <shivkumar.salunkhe@exaltaretech.com>
2023-06-12 00:02:09 +0530

    Mapped netflix data in cards & details page

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,9 +1,45 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
+import styles from "@/app/styles/common.module.css";
 
-function page() {
+const Page = async ({params})=> {
+  const id = params.id
+
+  const url = `https://netflix54.p.rapidapi.com/title/details/?ids=${id}&lang=en`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ad9af339d5msh21494400ef67793p175260jsndf4ccf089d7c",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const mainData = data[0].details;
+  console.log(mainData);
+
   return (
-    <h1>I am dynamic route</h1>
+    <>
+      <div className={styles.container}>
+        <h2 className={styles.movie_title}>Netflix \ <span>{mainData.type}</span></h2>
+        <div className={styles.card_section}>
+          <Image
+            src={mainData.backgroundImage.url}
+            alt={mainData.title}
+            height={400}
+            width={300}
+          />
+        </div>
+        <div>
+            <h1>{mainData.title}</h1>
+            <p>{mainData.synopsis}</p>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default page
+export default Page
--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,15 +1,37 @@
-import Link from 'next/link';
-import React from 'react';
+import React from "react";
+import MovieCard from "../components/MovieCard";
+import styles from "@/app/styles/common.module.css";
 
-const Page = () => {
-    return (
-        <>
-            <h1>This is movie Page</h1>
-            <Link href='/movie/shiv'>
-                Go To
-            </Link>
-        </>
-    );
+const Movie = async () => {
+  const url = process.env.RAPID_KEY;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ad9af339d5msh21494400ef67793p175260jsndf4ccf089d7c",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const mainData = data.titles;
+  console.log(mainData);
+
+  return (
+    <>
+      <section className={styles.movieSection}>
+        <div className={styles.container}>
+          <h1>Series & Movie</h1>
+          <div className={styles.card_section}>
+            {mainData.map((currElem) => {
+              return <MovieCard key={currElem.id} {...currElem} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
-export default Page;
+export default Movie;
